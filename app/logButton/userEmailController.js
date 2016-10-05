39c784cfd4381485ac5141eea4c72687bcd740e5
Juan José <juan.jose.arboleda.a@gmail.com>
2016-10-05 00:30:32 -0500

    Perdí mi tiempo, haha

--- app/logButton/userEmailController.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('logPage')
-    .controller('userEmailController', userEmailController);
-
-userEmailController.$inject = ['$scope'];
-
-function userEmailController($scope){
-
-}
