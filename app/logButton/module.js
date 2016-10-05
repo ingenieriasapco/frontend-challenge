46cecf94b02638abbacf152202190c6334aea51c
Juan José <juan.jose.arboleda.a@gmail.com>
2016-10-05 00:28:11 -0500

    Log in con github

---
+++ app/logButton/module.js
@@ -0,0 +1,5 @@
+angular
+    .module('logPage', [
+      'ngMaterial',
+      'ngAnimate'
+    ]);
---
+++ app/logButton/userEmailController.js
@@ -0,0 +1,9 @@
+angular
+    .module('logPage')
+    .controller('userEmailController', userEmailController);
+
+userEmailController.$inject = ['$scope'];
+
+function userEmailController($scope){
+
+}
