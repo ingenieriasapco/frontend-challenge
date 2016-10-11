346c4ba4803b88142dbef8106e68f3332ed48c20
daniel-990 <danielarango990@gmail.com>
2016-10-11 02:11:02 -0500

    proceso hasta el momento de la app

--- app/public/js/controller.js
+++ app/public/js/controller.js
@@ -1,7 +1,70 @@
+var config = {
+  apiKey: "AIzaSyD6c-yZn-gI_S6ysYE9hiUzxQng82KwyXU",
+  authDomain: "challenge-138b0.firebaseapp.com",
+  databaseURL: "https://challenge-138b0.firebaseio.com",
+  storageBucket: "challenge-138b0.appspot.com",
+  messagingSenderId: "866793699378"
+};
+firebase.initializeApp(config);
+var proveedor_github = new firebase.auth.GithubAuthProvider();
+
 angular.module('app.controllers', [])
-.controller('modulo-login', ['$scope', '$stateParams',
-  function ($scope, $stateParams){
+.controller('modulo-login', ['$scope', '$stateParams',function ($scope, $stateParams){
     $(document).ready(function(){
+      init();
       $('.tooltipped').tooltip({delay: 50});
     });
+
+    function init(){
+      firebase.auth().onAuthStateChanged(function(user){
+        if (user) {
+          console.log("el usuario esta conectado.");
+        } else {
+          console.log("el usuario esta desconectado.");
+        }
+      });
+
+      $("#ingresar-git").on('click',function(){
+        firebase.auth().signInWithPopup(proveedor_github).then(function(result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+          console.log("el usuario esta conectado " + user.displayName);
+          Materialize.toast("el usuario "+user.displayName+" esta conectado", 4000);
+        }).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          console.log(errorMessage);
+          Materialize.toast("error " + errorMessage, 4000);
+        });
+      })
+
+      $("#ingresar").on('click', function(){
+        var emailUser = $("#usuario").val();
+        var passUser = $("#pass").val();
+        firebase.auth().onAuthStateChanged(function(user){
+          if (user){
+            var correo = user.email;
+            console.log(correo);
+            Materialize.toast("el usuario "+correo+" esta conectado", 4000);
+            $("#usuario,#pass").val("");
+          } else {
+            console.log("usuario o conraseña erroneas!");
+            Materialize.toast("usuario o conraseña erroneas!", 4000);
+            $("#usuario,#pass").val("");
+          }
+        });
+        firebase.auth().signInWithEmailAndPassword(emailUser, passUser);
+      });
+
+      $("#cerrar-sesion").on('click',function(){
+        firebase.auth().signOut().then(function() {
+          Materialize.toast("usuario desconectado", 4000);
+        }, function(error) {
+          Materialize.toast("algo a salido mal. " +error , 4000);
+        });
+      })
+    }
   }])
+
