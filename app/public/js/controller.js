c703039b95fd8c5e60f9726cd992b59d2dd005c3
daniel-990 <danielarango990@gmail.com>
2016-10-11 23:26:41 -0500

    arch

--- app/public/js/controller.js
+++ app/public/js/controller.js
@@ -6,6 +6,7 @@ var config = {
   messagingSenderId: "866793699378"
 };
 firebase.initializeApp(config);
+var datosRutas = firebase.database();
 var proveedor_github = new firebase.auth.GithubAuthProvider();
 
 angular.module('app.controllers', [])
@@ -74,7 +75,7 @@ angular.module('app.controllers', [])
         if (user) {
           $("#usuario").html(user.displayName);
           console.log("el usuario esta conectado. ");
-          console.log(user);
+          //console.log(user);
         } else {
           window.location.href = 'http://'+URLdominio+'/#/login';
           console.log("el usuario esta desconectado.");
@@ -91,20 +92,157 @@ angular.module('app.controllers', [])
 
       $http.get('../json/rutas.json')
        .then(function(res){
-          $scope.rutasMed = res.data;  
-          console.log($scope.rutasMed = res.data);              
+          $scope.rutasMed = res.data; 
+          var json = $scope.rutasMed = res.data; 
+          //console.log($scope.rutasMed = res.data);             
+          datosRutas.ref('datosRutas').update({
+            'sector1':{
+              'nombreConductor': res.data[0].nombreConductor,'nombreSector': res.data[0].nombreRuta,'nombreFlota': res.data[0].nombreFlota,
+              'ruta1': {
+                'nombreRuta': res.data[0].nombrePunto.ruta1.nombre,
+                'tiempoRuta': res.data[0].nombrePunto.ruta1.tiempo,
+                'longitud': res.data[0].nombrePunto.ruta1.longitud,
+                'latitud': res.data[0].nombrePunto.ruta1.latitud
+              },
+              'ruta2':{
+                'nombreRuta': res.data[0].nombrePunto.ruta2.nombre,
+                'tiempoRuta': res.data[0].nombrePunto.ruta2.tiempo,
+                'longitud': res.data[0].nombrePunto.ruta2.longitud,
+                'latitud': res.data[0].nombrePunto.ruta2.latitud
+              },
+              'ruta3':{
+                'nombreRuta': res.data[0].nombrePunto.ruta3.nombre,
+                'tiempoRuta': res.data[0].nombrePunto.ruta3.tiempo,
+                'longitud': res.data[0].nombrePunto.ruta3.longitud,
+                'latitud': res.data[0].nombrePunto.ruta3.latitud
+              }
+            },
+            'sector2':{
+              'nombreConductor': res.data[1].nombreConductor,'nombreSector': res.data[1].nombreRuta,'nombreFlota': res.data[1].nombreFlota,
+              'ruta1': {
+                'nombreRuta': res.data[1].nombrePunto.ruta1.nombre,
+                'tiempoRuta': res.data[1].nombrePunto.ruta1.tiempo,
+                'longitud': res.data[1].nombrePunto.ruta1.longitud,
+                'latitud': res.data[1].nombrePunto.ruta1.latitud
+              },
+              'ruta2':{
+                'nombreRuta': res.data[1].nombrePunto.ruta2.nombre,
+                'tiempoRuta': res.data[1].nombrePunto.ruta2.tiempo,
+                'longitud': res.data[1].nombrePunto.ruta2.longitud,
+                'latitud': res.data[1].nombrePunto.ruta2.latitud
+              },
+              'ruta3':{
+                'nombreRuta': res.data[1].nombrePunto.ruta3.nombre,
+                'tiempoRuta': res.data[1].nombrePunto.ruta3.tiempo,
+                'longitud': res.data[1].nombrePunto.ruta3.longitud,
+                'latitud': res.data[1].nombrePunto.ruta3.latitud
+              }
+            },
+            'sector3':{
+              'nombreConductor': res.data[2].nombreConductor,'nombreSector': res.data[2].nombreRuta,'nombreFlota': res.data[2].nombreFlota,
+              'ruta1': {
+                'nombreRuta': res.data[2].nombrePunto.ruta1.nombre,
+                'tiempoRuta': res.data[2].nombrePunto.ruta1.tiempo,
+                'longitud': res.data[2].nombrePunto.ruta1.longitud,
+                'latitud': res.data[2].nombrePunto.ruta1.latitud
+              },
+              'ruta2':{
+                'nombreRuta': res.data[2].nombrePunto.ruta2.nombre,
+                'tiempoRuta': res.data[2].nombrePunto.ruta2.tiempo,
+                'longitud': res.data[2].nombrePunto.ruta2.longitud,
+                'latitud': res.data[2].nombrePunto.ruta2.latitud
+              },
+              'ruta3':{
+                'nombreRuta': res.data[2].nombrePunto.ruta3.nombre,
+                'tiempoRuta': res.data[2].nombrePunto.ruta3.tiempo,
+                'longitud': res.data[2].nombrePunto.ruta3.longitud,
+                'latitud': res.data[2].nombrePunto.ruta3.latitud
+              }
+            }
+          });
         });
-  }
+}
+
 }])
 .controller('modulo-mapas', ['$scope', '$stateParams',function ($scope, $stateParams){
+  $(document).ready(function(){
+    inicioMap();
+    googleMap();
+    $('.tooltipped').tooltip({delay: 50});
+  })
+  function inicioMap(){
       firebase.auth().onAuthStateChanged(function(user){
         var URLdominio = window.location.host;
-        if (user){
+        if (user) {
+          $("#usuario").html(user.displayName);
           console.log("el usuario esta conectado. ");
         } else {
           window.location.href = 'http://'+URLdominio+'/#/login';
           console.log("el usuario esta desconectado.");
         }
       });
+      $("#cerrar-sesion").on('click',function(){
+        firebase.auth().signOut().then(function(user){
+          Materialize.toast("usuario desconectado", 4000);
+        }, function(error) {
+          Materialize.toast("algo a salido mal. " +error , 4000);
+        });
+      })
+  }
+  function googleMap(){
+    if (navigator.geolocation) {
+      console.log("el navegador es geolocalizable");
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var miPos = position;
+        var lati = parseFloat(miPos.coords.latitude);
+        var long = parseFloat(miPos.coords.longitude);
+
+        console.log(miPos.coords.latitude + "\n" + miPos.coords.longitude);
+
+        var myLatLng = { lat: lati, lng: long };
+        var destino = { lat:4.6482837, lng: -74.2478954 }
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+          zoom: 7
+        });
+        var marker = new google.maps.Marker({
+          map: map,
+          position: myLatLng,
+          title: 'aqui estoy!'
+        });
+
+        var objConfigDR = {
+          mapa: map 
+        }
+        
+        var objConfigDS = {
+          origin:myLatLng,
+          destination: destino,
+          travelMode: google.maps.TravelMode.DRIVING
+        }
+
+        var ds = new google.maps.DirectionsService();
+        var dr = new google.maps.DirectionsRenderer(objConfigDR);
+
+        ds.route(objConfigDS, fnRutear);
+
+        function fnRutear(resultados, status){
+          //mostrar la ruta entre a y b
+          if(status == 'OK'){
+            dr.setDirections(resultados);
+            console.log("correcto!");
+          }else{
+            console.log("error " + status);
+          }
+        }
+
+      });// funcion principal
+    } else {
+      console.log("el navegador no es geolocalizable");
+    }
+
+  }//function principal
+
 }])
 
