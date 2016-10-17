2588241dbc3d3ccafe161afd3b0561d604181686
maomusic <maopro16@hotmail.com>
2016-10-17 14:29:28 -0500

    Adding ngroute and ngresource and routes in app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
-angular.module('routesApp', []);
+angular.module('routesApp', ['ngResource',"ngRoute"])
+.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider){
+	$routeProvider
+	.when('/',{
+		controller: "login",
+		templateUrl: "/index.html",
+	
+	});
+    $routeProvider.when('/routes.html',
+            {
+                templateUrl: 'routes.html',
+                controller: 'routes'                
+            });
+    $routeProvider.when('/routes.html/:routeId',
+            {
+                templateUrl: 'routeDetails.html',
+                controller: 'routeDetails',
+            }).        
+	otherwise({
+		redirectTo: '/'});
+}]);
