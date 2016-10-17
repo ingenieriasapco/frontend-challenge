7e0d49726c8f73c4e917b9243bf60f3071b60969
maomusic <maopro16@hotmail.com>
2016-10-17 18:03:26 -0500

    Adding auth factory and fixing the authentication controler

--- app/js/controllers/userAuthentication.js
+++ app/js/controllers/userAuthentication.js
@@ -2,18 +2,63 @@
 
     'use strict';
 
-    userAuthentication.$inject = ['$firebaseAuth','$rootScope'];
-
+    userAuthentication.$inject = 
+    [
+        'auth',
+        '$location'
+       
+    ];
     
+    function userAuthentication(auth,$location){
 
-    function userAuthentication($firebaseAuth,$rootScope){
+        var vm=this;
+        vm.provider='';
+        vm.authData;
 
-        $rootScope.$auth = $firebaseAuth(new Firebase('https://maoapp-befbd.firebaseio.com'));
-        $rootScope.$auth.$onAuth(function(authData){
-            $rootScope.authData = authData;
-            });
+      auth.$onAuth(function(authData){
+		vm.authData = authData;
+		if(authData) {
+			vm.cachedProfile = getCachedProfile();
+			createUser();
+			//$location.path("/authenticated");
+		}
+		console.log(vm.authData);
+	});
+
+    vm.login=function(provider)  {
+
+        auth.$authWithOAuthPopup(provider,  { scope: 'email' })
+		.catch(function(error){
+			console.error(error);
+		})
 
     }
+
+
+    }
+    
+    
+	/*
+
+	var getCachedProfile = function() {
+		if(!$scope.authData) return "";
+
+		switch($scope.authData.provider) {
+			case "github":
+				return $scope.authData.github.cachedUserProfile;
+				break;
+			case "facebook":
+				return $scope.authData.facebook.cachedUserProfile;
+				break;
+			case "google":
+				return $scope.authData.google.cachedUserProfile;
+				break;
+			default:
+				return "";
+		}
+	}  */
+
+
 
     angular.module('routesApp').controller('userAuthentication',userAuthentication);
 
