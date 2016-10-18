a3a6494810f3cc67753f3face4698cebc2792e2f
maomusic <maopro16@hotmail.com>
2016-10-17 23:57:24 -0500

    Loading places in map, missing draw the routes

--- app/js/controllers/routeDetails.js
+++ app/js/controllers/routeDetails.js
@@ -7,24 +7,38 @@
 
     var vm = this;
     vm.toggleSelection = toggleSelection;
+    vm.routes=routeData.routes;
     vm.selection=[];
-    
-    vm.positions =[ 
-        { title: 'Path 1', pos:[40.71, -74.21] }, 
-        { title: 'Path 2', pos:[40.72, -74.20] }, 
-        { title: 'Path 3', pos:[40.73, -74.19] }
-    ];
+    vm.positions=[];
+
+
+    angular.forEach(vm.routes[0].paths, function(path) {
+
+             vm.positions.push({name:path.name,pos:[path.lat,path.lng]});
+
+                          
+             });              
         
     function toggleSelection(position) {
+
         var idx = vm.selection.indexOf(position);
         if (idx > -1) {
         vm.selection.splice(idx, 1);
+        
         }
         // is newly selected
         else {
         vm.selection.push(position);
         }
     }
+
+    /*
+
+     vm.markers = [{ id: 1, lat: 37.772323, lng: -122.214897 }, { id: 2, lat: 21.291982, lng: -157.821856 }, { id: 3, lat: -27.46758, lng: 153.027892 }];
+    vm.path = vm.markers.map(function(marker){
+        return [marker.lat,marker.lng];
+    });  */
+
     }
 
     angular.module('routesApp').controller('routeDetails',routeDetails);
